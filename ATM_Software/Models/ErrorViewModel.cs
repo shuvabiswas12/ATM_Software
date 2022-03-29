bfbd8ecd1347189aff87a5779723611dbc5401e8
Shuva Biswas <shuvabiswas12@gmail.com>
2022-03-29 10:55:26 +0600

    ATM_Software using dotnet core 5 mvc

---
+++ ATM_Software/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ATM_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
