b944c347d62f2739fbb7870bec93b9a3336a5d87
Shuva Biswas <shuvabiswas12@gmail.com>
2022-05-17 02:16:59 +0600

    added cash's ui, shared sidenav bar.

---
+++ ATM_Software/Controllers/CashTransferController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATM_Software.Controllers
+{
+    public class CashTransferController : Controller
+    {
+        public IActionResult Transfer()
+        {
+            ViewBag.Current = "Transfer";
+            return View();
+        }
+    }
+}
---
+++ ATM_Software/Controllers/WithdrawCashController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATM_Software.Controllers
+{
+    public class WithdrawCashController : Controller
+    {
+        public IActionResult Withdraw()
+        {
+            ViewBag.Current = "Withdraw";
+            return View();
+        }
+    }
+}
