5a5bc30e931aeacefdcf4f9c487d80f324cab335
Shuva Biswas <shuvabiswas12@gmail.com>
2022-05-18 13:49:56 +0600

    added admin nav and create a new user form ui.

---
+++ ATM_Software/Controllers/Admin/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATM_Software.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAccount() { return View(); }
+
+        public IActionResult DeleteAccount() { return View(); }
+
+        public IActionResult UpdateAccount() { return View(); }
+
+        public IActionResult ViewReport() { return View(); }
+
+        public IActionResult UpdateAdminCredentials() { return View(); }
+    }
+}
